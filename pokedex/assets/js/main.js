ec1f14ee061f08a10fb4a8b69d6fdc2150516664
robgol1133 <c.robson1133@gmail.com>
2022-11-17 23:19:17 -0300

    Manipulando múltiplas requisições em paralelo

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,17 +1,20 @@
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
 
 function convertPokemonToLi(pokemon) {
     return `
             <li class="pokemon">
-            <span class="number">#001</span>
+            <span class="number">${pokemon.order}</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
                 <ol class="types">
-                    <li class="type">grass</li>
-                    <li class="type">poison</li>
+                    ${convertPokemonTypesToLi(pokemon.types).join('')}
                 </ol>
 
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                <img src="${pokemon.sprites.other.dream_world.front_default}"
                     alt="${pokemon.name}">
             </div>
 
@@ -22,5 +25,6 @@ function convertPokemonToLi(pokemon) {
 const pokemonList = document.getElementById('pokemonList');
 
 pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
+    const neWHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = neWHtml
 })
